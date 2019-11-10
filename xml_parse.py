45b2dd3a9ac0bd35836a7521cf018c29bc17aa6f
rainermesi <rainer.mesi@gmail.com>
2019-11-10 18:59:15 +0200

    cleaned code for github

--- xml_parse.py
+++ xml_parse.py
@@ -1,49 +1,11 @@
+# import libs
 import os
 import xml.etree.ElementTree as ET
 import pandas
 
-#setwd
-
-os.chdir('C:\\Users\\raine\\Google Drive\\DATA\\Tableau DataSource')
-os.getcwd()
-os.listdir()
-
-#parse xml
-
+# get and parse xml
 tree = ET.parse('b2b_loyalty.xml')
-
 root = tree.getroot()
-    
-fields = {
-        'name': [],
-        'caption': [],
-        'role': [],
-        'datatype': [],
-        'formula': []
-        }
-
-#fields['name'].append('me')
-
-for column in root.iter('column'):
-    fields['name'].append(column.attrib['name'])
-    if 'caption' in column.attrib:
-        fields['caption'].append(column.attrib['caption'])
-    else:
-        fields['caption'].append('no caption')
-    fields['role'].append(column.attrib['role'])
-    fields['datatype'].append(column.attrib['datatype'])
-    if 'formula' in column.attrib:
-        for calculation in column.iter('calculation'):
-            fields['formula'].append(calculation.attrib['formula'])
-    else: fields['formula'].append('!not a calculated field!')
-
-#if statemetn does not work, always returns else
-        
-fields_df = pandas.DataFrame(fields)
-
-fields_df.head()
-
-fields_df.to_csv('parse_result.txt', sep = '|')
 
 #########################################################################
 ## DEBUG START
