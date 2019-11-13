c3795e7e1e0315d2d7686a48fc8dcfc54702358f
rainermesi <rainer.mesi@gmail.com>
2019-11-13 22:06:48 +0200

    Converted the main script to a function. Created xml_parse_func.py file

---
+++ xml_parse_func.py
@@ -0,0 +1,53 @@
+import os
+import xml.etree.ElementTree as ET
+import pandas
+
+def xml_parse(xmlfile):
+    col_names = {
+        'colname': [],
+        'datatype': [],
+        'role': []
+    }
+    col_meta = {
+        'colname': [],
+        'class': [],
+        'formula': []
+    }
+    data_meta = {
+    'remote-name': [],
+    'local-name': [],
+    'parent-name': [],
+    }
+
+    xmlroot = ET.parse(xmlfile).getroot()
+    xmliterator = xmlroot.getiterator()
+
+    for column in root.iter('column'):
+        col_names['colname'].append(column.attrib['name'])
+        col_names['datatype'].append(column.attrib['datatype'])
+        col_names['role'].append(column.attrib['role'])
+        for i in column:
+            if i.attrib == {}:
+                col_meta['colname'].append(column.attrib['name'])
+                col_meta['class'].append('not applicable')
+                col_meta['formula'].append('not applicable')
+            else:
+                col_meta['colname'].append(column.attrib['name'])
+                col_meta['class'].append(i.attrib['class'])
+                col_meta['formula'].append(i.attrib['formula'])
+
+    for element in xmliterator:
+        if element.tag == 'metadata-record':
+            if list(element):
+                for child in element:
+                    if child.tag == 'remote-name':
+                        data_meta['remote-name'].append(child.text)
+                    if child.tag == 'local-name':
+                        data_meta['local-name'].append(child.text)
+                    if child.tag == 'parent-name':
+                        data_meta['parent-name'].append(child.text)
+
+    pandas.DataFrame(col_names).merge(pandas.DataFrame(col_meta), on='colname', how='left' ).to_csv(r'C:\Users\raine\Google Drive\DATA\Tableau DataSource\columns.csv')
+    pandas.DataFrame(data_meta).to_csv(r'C:\Users\raine\Google Drive\DATA\Tableau DataSource\metadata.csv')
+
+
